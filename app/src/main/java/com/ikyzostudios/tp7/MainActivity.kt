9cd11ec1abdffa1a440eb5f2f5ebf562bcb06197
AchrefNa <nabli95@hotmail.com>
2023-10-31 20:54:36 +0100

    commit 3

--- app/src/main/java/com/ikyzostudios/tp7/MainActivity.kt
+++ app/src/main/java/com/ikyzostudios/tp7/MainActivity.kt
@@ -2,10 +2,67 @@ package com.ikyzostudios.tp7
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.ikyzostudios.tp7.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(
+            this, R.layout.activity_main)
+
+
+
+                binding.button6.setOnClickListener{
+                    if (binding.num1.text.toString().isNotEmpty() &&
+                        binding.num2.text.toString().isNotEmpty()
+                    ) {
+
+                    binding.result.setText((binding.num1.text.toString().toInt() + binding.num2.text.toString().toInt()).toString())
+                }
+
+
+            }
+
+        binding.button7.setOnClickListener{
+            if (binding.num1.text.toString().isNotEmpty() &&
+                binding.num2.text.toString().isNotEmpty()
+            ) {
+
+                binding.result.setText((binding.num1.text.toString().toInt() - binding.num2.text.toString().toInt()).toString())
+            }
+
+
+        }
+        binding.button8.setOnClickListener{
+            if (binding.num1.text.toString().isNotEmpty() &&
+                binding.num2.text.toString().isNotEmpty()
+            ) {
+
+                binding.result.setText((binding.num1.text.toString().toInt() * binding.num2.text.toString().toInt()).toString())
+            }
+
+
+        }
+        binding.button9.setOnClickListener{
+            if (binding.num1.text.toString().isNotEmpty() &&
+                binding.num2.text.toString().isNotEmpty()&&
+                binding.num2.text.toString() !="0"
+            ) {
+
+                binding.result.setText((binding.num1.text.toString().toInt() / binding.num2.text.toString().toInt()).toString())
+            }
+
+
+
+
+        }
+
+                binding.clear.setOnClickListener {
+                    binding.num1.text=null
+                    binding.num2.text=null
+            }
+
     }
 }
